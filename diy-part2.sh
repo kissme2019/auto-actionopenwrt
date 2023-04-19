971477163cc9d60a587d6698faf63fe26dcc1a16
kissme2019 <59078384+kissme2019@users.noreply.github.com>
2023-04-19 08:13:56 +0800

    Update diy-part2.sh
    新增修复编译错误的

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.2.8/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# repatch kenso8's naiveproxy patchfiles
+sed -i 's/if (is_linux) {/if (is_linux || is_chromeos) {/' package/feeds/kenzo/naiveproxy/patches/100-build-add-OpenWrt-staging-libraries.patch
+sed -i 's/@@ -746,7 +746,6/@@ -751,7 +751,6/' package/feeds/kenzo/naiveproxy/patches/101-build-drop-useless-deps-simd_asm.patch
